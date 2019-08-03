71b5cc3cdde35d08dc43ce83f73bb9307328dcd8
Tung Hoang <tungh@mobio.vn>
2019-08-03 13:04:42 +0700

    add handle error

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -44,6 +45,24 @@ export class AppService {
                 'UID': 'a5bbd1f807b3c5e069a0f002a915abd9',
             })
         };
-        return this.http.post<any>(url, body, httpOptions);
+        return this.http.post<any>(url, body, httpOptions).pipe(
+            catchError(AppService.handleError)
+        );
     }
+
+    private static handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            console.error(
+                `Backend returned code ${error.status}, ` +
+                `body was: ${error.error}`);
+        }
+        // return an observable with a user-facing error message
+        return throwError(
+            'Something bad happened; please try again later.');
+    };
 }
